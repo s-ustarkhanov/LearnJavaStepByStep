fdd612d05e14263612bd362d5900bc74b336ce71
Shisa Ustarkhanov <shisaustarkhanov@gmail.com>
2024-01-28 14:45:02 +0400

    Completed Task-10

--- JavaTasks/src/task10/Main.java
+++ JavaTasks/src/task10/Main.java
@@ -2,7 +2,7 @@ package task10;
 
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите целое число: ");
